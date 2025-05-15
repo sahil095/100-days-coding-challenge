f2978e4c7d2ad3e90fa8502c0cdfbd406e7ce3b2
Sahil Sehgal <Sehgal.sahil0786@gmail.com>
2025-05-15 01:45:43 -0400

    Add files via upload

---
+++ LeetCode/408. Valid Word Abbreviation.py
@@ -0,0 +1,18 @@
+def validWordAbbreviation(word, abbr):
+	i = j = 0
+	
+	while i < len(word) and j < len(abbr):
+		if abbr[j].isdigit():
+			if abbr[j] == '0':
+				return False
+			num = 0
+			while j < len(abbr) and abbr[j].isdigit():
+				num = num * 10 + int(abbr[j])
+				j += 1
+			i += num
+		else:
+			if word[i] != abbr[j] and i >= len(word):
+					return False
+			i += 1
+			j += 1
+	return i == len(word) and j == len(abbr)
